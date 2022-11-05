82d3bb348f04d2cb247fce4eef4e39c20f436899
kpodjison <jeevista@gmail.com>
2022-11-06 01:59:11 +0800

    Latest product sections added

--- src/js/index.js
+++ src/js/index.js
@@ -12,10 +12,9 @@ $(document).ready(function(){
 
     // owl carousel 
     $("#hot-deals .owl-carousel").owlCarousel({
+        nav:true,
         loop:true,
         margin: 10,
-        dots:true,
-        nav:true,
         responsive: {
             0:{
                 items:1
