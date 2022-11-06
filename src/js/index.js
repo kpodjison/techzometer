ab6e671603ddd409f3b0b23b2457b9b34b98dc90
kpodjison <jeevista@gmail.com>
2022-11-06 18:51:23 +0800

    Advert and comming soon sections added

--- src/js/index.js
+++ src/js/index.js
@@ -27,4 +27,23 @@ $(document).ready(function(){
             }
         }
     });
+
+
+    // owl carousel 
+    $("#coming-soon .owl-carousel").owlCarousel({
+        nav:true,
+        loop:true,
+        margin: 10,
+        responsive: {
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });
